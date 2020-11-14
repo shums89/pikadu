97d4675d9a405a83c1f703fbf194b23c2d24d62f
Александр <sh889@ya.ru>
2020-11-14 12:30:49 +0500

    День 1. Регистрация и авторизация

--- js/main.js
+++ js/main.js
@@ -5,3 +5,85 @@ menuToggle.addEventListener('click', function (evt) {
   evt.preventDefault();
   menu.classList.toggle('visible');
 })
+
+const loginElem = document.querySelector('.login');
+const loginForm = document.querySelector('.login-form');
+const emailInput = document.querySelector('.login-email');
+const passwordInput = document.querySelector('.login-password');
+const loginSignup = document.querySelector('.login-signup');
+
+const userElem = document.querySelector('.user');
+const userNameElem = document.querySelector('.user-name');
+
+const listUsers = [
+  {
+    id: '01',
+    email: 'maks@mail.com',
+    password: '12345',
+    displayName: 'MaksJS'
+  },
+  {
+    id: '02',
+    email: 'kate@mail.com',
+    password: '123456',
+    displayName: 'KateKillMaks'
+  }
+];
+
+const setUsers = {
+  user: null,
+  logIn(email, password, handler) {
+    const user = this.getUser(email);
+    if (user && user.password === password) {
+      this.authorizedUser(user);
+      handler();
+    } else {
+      alert('Пользователь с такиими данными не найден');
+    }
+  },
+  logOut() {
+
+  },
+  signUp(email, password, handler) {
+    if (!this.getUser(email)) {
+      const user = { email, password, displayName: email };
+
+      listUsers.push(user);
+      this.authorizedUser(user);
+      handler();
+    } else {
+      alert('Пользователь с таким email уже зарегистрирован');
+    }
+  },
+  getUser(email) {
+    return listUsers.find(item => item.email === email);
+  },
+  authorizedUser(user) {
+    this.user = user;
+  }
+};
+
+const toggleAuthDom = () => {
+  const user = setUsers.user;
+
+  if (user) {
+    loginElem.style.display = 'none';
+    userElem.style.display = '';
+    userNameElem.textContent = user.displayName;
+  } else {
+    loginElem.style.display = '';
+    userElem.style.display = 'none';
+  }
+}
+
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+  setUsers.logIn(emailInput.value, passwordInput.value, toggleAuthDom);
+});
+
+loginSignup.addEventListener('click', event => {
+  event.preventDefault();
+  setUsers.signUp(emailInput.value, passwordInput.value, toggleAuthDom);
+});
+
+toggleAuthDom();
