8d6124389a21ae308a5ac754d701b57ec1d9e3d3
Александр <sh889@ya.ru>
2020-11-14 11:27:31 +0500

    Подготовка верстки

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ const menuToggle = document.querySelector('#menu-toggle');
 const menu = document.querySelector('.sidebar');
 
 menuToggle.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    menu.classList.toggle('visible');
+  evt.preventDefault();
+  menu.classList.toggle('visible');
 })
