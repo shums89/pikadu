6cfe9d620e78eb0f74c37fa226cde6e643717d6a
Александр <sh889@ya.ru>
2020-11-17 03:06:00 +0500

    День 3. Добавление постов

--- js/main.js
+++ js/main.js
@@ -15,12 +15,13 @@ const userNameElem = document.querySelector('.user-name');
 const exitElem = document.querySelector('.exit');
 const editElem = document.querySelector('.edit');
 const editContainer = document.querySelector('.edit-container');
-
 const editUserName = document.querySelector('.edit-username');
 const editPhotoURL = document.querySelector('.edit-photo');
 const userAvatarElem = document.querySelector('.user-avatar');
 
 const postWrapper = document.querySelector('.posts');
+const buttonNewPost = document.querySelector('.button-new-post');
+const addPostElem = document.querySelector('.add-post');
 
 const listUsers = [
   {
@@ -103,7 +104,7 @@ const setPosts = {
       title: 'Заголовок поста',
       text: 'Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные тексты. То буквенных однажды продолжил не, диких наш жизни встретил живет большой предупреждал единственное что! Даже ipsum необходимыми океана свой от всех, эта, ты переписывается, они вопрос маленький подзаголовок lorem реторический свое коварный обеспечивает жаренные. Гор назад продолжил дороге первую живет это ему вдали, семь инициал последний встретил за рукопись текста обеспечивает своих он алфавит великий всемогущая силуэт? Взобравшись языком решила переписывается заманивший о жаренные пустился большого курсивных, запятой текстами лучше там живет ее, диких домах. Эта великий рот текстами имеет буквоград переулка путь единственное не взгляд алфавит.',
       tags: ['новое', 'горячее', 'мое', 'свежее', 'случайность'],
-      author: 'kate@mail.com',
+      author: { displayName: 'kate', photo: 'https://shutniki.club/wp-content/uploads/2020/04/Multyashnye_kartinki_na_avu_1_05115443.jpg' },
       date: '11.11.2020, 20:25:00',
       likes: 15,
       comments: 20
@@ -112,12 +113,30 @@ const setPosts = {
       title: 'Заголовок поста2',
       text: 'Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные тексты. То буквенных однажды продолжил не, диких наш жизни встретил живет большой предупреждал единственное что! Даже ipsum необходимыми океана свой от всех, эта, ты переписывается, они вопрос маленький подзаголовок lorem реторический свое коварный обеспечивает жаренные. Гор назад продолжил дороге первую живет это ему вдали, семь инициал последний встретил за рукопись текста обеспечивает своих он алфавит великий всемогущая силуэт? Взобравшись языком решила переписывается заманивший о жаренные пустился большого курсивных, запятой текстами лучше там живет ее, диких домах. Эта великий рот текстами имеет буквоград переулка путь единственное не взгляд алфавит.',
       tags: ['новое', 'горячее', 'мое', 'свежее', 'случайность'],
-      author: 'maks@mail.com',
+      author: { displayName: 'maks', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSPtb0AVxmOo-qxteP5seN3tpc4OG3FIJ_JA&usqp=CAU' },
       date: '11.11.2020, 20:25:00',
       likes: 45,
       comments: 12
     }
-  ]
+  ],
+  addPost(title, text, tags, handler) {
+    this.allPosts.unshift({
+      title,
+      text,
+      tags: tags.split(',').map(item => item.trim()),
+      author: {
+        displayName: setUsers.user.displayName,
+        photo: setUsers.user.photo
+      },
+      date: new Date().toLocaleString(),
+      likes: 0,
+      comments: 0,
+    });
+
+    if (handler) {
+      handler();
+    }
+  }
 }
 
 function toggleAuthDom() {
@@ -128,10 +147,19 @@ function toggleAuthDom() {
     userElem.style.display = '';
     userNameElem.textContent = user.displayName;
     userAvatarElem.src = user.photo || userAvatarElem.src;
+    buttonNewPost.classList.add('visible');
   } else {
     loginElem.style.display = '';
     userElem.style.display = 'none';
+    buttonNewPost.classList.remove('visible');
+    addPostElem.classList.remove('visible');
+    postWrapper.classList.add('visible');
   }
+}
+
+function showAddPost() {
+  addPostElem.classList.add('visible');
+  postWrapper.classList.remove('visible');
 }
 
 function showAllPosts() {
@@ -143,7 +171,7 @@ function showAllPosts() {
         <div class="post-body">
           <h2 class="post-title">${title}</h2>
           <p class="post-text">${text}</p>
-          <div class="tags">${tags}</div>
+          <div class="tags">${tags.map(tag => `<a href="#" class="tag">#${tag}</a>`)}</div>
         </div>
         <div class="post-footer">
           <div class="post-buttons">
@@ -172,10 +200,10 @@ function showAllPosts() {
           </div>
           <div class="post-author">
             <div class="author-about">
-              <a href="#" class="author-username">${author}</a>
+              <a href="#" class="author-username">${author.displayName}</a>
               <span class="post-time">${date}</span>
             </div>
-            <a href="#" class="author-link"><img src="img/avatar.jpeg" alt="" class="author-avatar"></a>
+            <a href="#" class="author-link"><img src=${author.photo || "img/avatar.jpeg"} alt="" class="author-avatar"></a>
           </div>
         </div>
       </section>
@@ -183,6 +211,9 @@ function showAllPosts() {
   });
 
   postWrapper.innerHTML = postsHTML;
+
+  addPostElem.classList.remove('visible');
+  postWrapper.classList.add('visible');
 }
 
 function init() {
@@ -220,6 +251,31 @@ function init() {
     evt.preventDefault();
     menu.classList.toggle('visible');
   })
+
+  buttonNewPost.addEventListener('click', event => {
+    event.preventDefault();
+    showAddPost();
+  });
+
+  addPostElem.addEventListener('submit', event => {
+    event.preventDefault();
+    const { title, text, tags } = addPostElem.elements;
+
+    if (title.value.length < 6) {
+      alert('Слишком короткий заголовок');
+      return;
+    }
+
+    if (text.value.length < 50) {
+      alert('Слишком короткий пост');
+      return;
+    }
+
+    setPosts.addPost(title.value, text.value, tags.value, showAllPosts);
+
+    addPostElem.classList.remove('visible');
+    addPostElem.reset();
+  });
 
   showAllPosts();
   toggleAuthDom();
